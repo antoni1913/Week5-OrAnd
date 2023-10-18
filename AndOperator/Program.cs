2f75a46a93057be76bea5d78ce4bc0eef04aa6b1
antoni1913 <antonisariev13@gmail.com>
2023-10-19 02:08:44 +0300

    week5

---
+++ AndOperator/Program.cs
@@ -0,0 +1,34 @@
+﻿// 0 or below - freezing
+// 0 to  5 - cold
+// 5 to 10 - chilly
+// 10 - 15 - cool
+// 15 to 28 - warm 
+// 28 above - boiling hot 
+
+Console.WriteLine("Enter the temperature");
+int currentTemp = Int32.Parse(Console.ReadLine());
+
+if (currentTemp <= 0)
+{
+    Console.WriteLine("Freezing");
+}
+else if (currentTemp >= 0 && currentTemp < 5)
+{
+    Console.WriteLine("Cold");
+}
+else if (currentTemp >= 5 && currentTemp < 10)
+{
+    Console.WriteLine("chilly");
+}
+else if (currentTemp >= 10 && currentTemp < 15)
+{
+    Console.WriteLine("Cool");
+}
+else if (currentTemp >= 15 && currentTemp < 28)
+{
+    Console.WriteLine("Warm");
+}
+else
+{
+    Console.WriteLine("HOT");
+}
---
+++ week5AndOr/Program.cs
@@ -0,0 +1,35 @@
+﻿// Or && And
+
+string username, password;
+
+Console.WriteLine("enter name");
+username = Console.ReadLine();
+
+Console.WriteLine("enter pass");
+password = Console.ReadLine();
+
+
+//OR
+
+
+if (username != "admin" || password != "admin123")
+{
+    Console.WriteLine("WRONG");
+}
+else
+{
+    Console.WriteLine("Wellcome!");
+}
+
+
+
+
+
+//if (username == "admin" && password == "admin123")
+//{
+//    Console.WriteLine("wellcome");
+//}
+//else
+//{
+//    Console.WriteLine("WRONG");
+//}
